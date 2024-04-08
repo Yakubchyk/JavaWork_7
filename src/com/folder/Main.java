b83a3750fe13d67b43e8259789e21c7b7f35551d
Yakubchyk <ivan.yakubchyk@gmail.com>
2024-04-08 19:51:59 +0200

    wolf

---
+++ src/com/folder/Main.java
@@ -0,0 +1,10 @@
+package com.folder;
+import folder.Wolf;
+
+public class Main {
+    public static void main(String[] args) {
+        Wolf wolf = new Wolf();
+        wolf
+
+    }
+}
---
+++ src/folder/Wolf.java
@@ -0,0 +1,15 @@
+package folder;
+
+import lombok.*;
+
+@Getter
+@Setter
+public class Wolf {
+    private int age;
+    private double weight;
+    private String name;
+    //get set
+
+}
+
+
